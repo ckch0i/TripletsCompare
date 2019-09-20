b2e00948ee9923b23ec048179008ad53c7d8c033
ckchoi <ck.choi@capgemini.com>
2019-09-20 14:47:53 +0200

    first commit

---
+++ src/main/java/com/capgemini/App.java
@@ -0,0 +1,27 @@
+package com.capgemini;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        return new ArrayList<>(Arrays.asList(2,2));
+    }
+
+    //method to determine who wins.
+    static void determineWhoWins ()
+
+}
